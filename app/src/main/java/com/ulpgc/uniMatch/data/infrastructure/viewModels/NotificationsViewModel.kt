65742b0a1e2ee6c3da53d3c035f99f795f18bc46
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-11-05 14:15:17 +0000

    Notifications ViewModel

--- app/src/main/java/com/ulpgc/uniMatch/data/infrastructure/viewModels/NotificationsViewModel.kt
+++ app/src/main/java/com/ulpgc/uniMatch/data/infrastructure/viewModels/NotificationsViewModel.kt
@@ -1,7 +1,12 @@
 package com.ulpgc.uniMatch.data.infrastructure.viewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ulpgc.uniMatch.data.application.services.NotificationsService
+import com.ulpgc.uniMatch.data.domain.models.notification.Notifications
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class NotificationsViewModel (
     private val notificationsService: NotificationsService,
@@ -9,4 +14,60 @@ class NotificationsViewModel (
     private val authViewModel: AuthViewModel
 ) : ViewModel() {
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> get() = _isLoading
+
+    private val _notifications = MutableStateFlow<List<Notifications>>(emptyList())
+    val notifications: StateFlow<List<Notifications>> get() = _notifications
+
+    fun loadNotifications() {
+        viewModelScope.launch {
+            _isLoading.value = true
+            val result = authViewModel.userId?.let { notificationsService.getNotifications(it) }
+            if (result != null) {
+                result.onSuccess { notifications ->
+                    _notifications.value = notifications
+                    _isLoading.value = false
+                }.onFailure { error ->
+                    errorViewModel.showError(
+                        error.message ?: "Error loading notifications"
+                    )
+                    _isLoading.value = false
+                }
+            }
+        }
+    }
+
+    fun markNotificationAsRead(notificationId: String) {
+        viewModelScope.launch {
+            val result = notificationsService.markNotificationAsRead(notificationId)
+            result.onFailure { error ->
+                errorViewModel.showError(
+                    error.message ?: "Error marking notification as read"
+                )
+            }
+        }
+    }
+
+    fun deleteNotification(notificationId: String) {
+        viewModelScope.launch {
+            val result = notificationsService.deleteNotification(notificationId)
+            result.onFailure { error ->
+                errorViewModel.showError(
+                    error.message ?: "Error deleting notification"
+                )
+            }
+        }
+    }
+
+    fun deleteAllNotifications() {
+        viewModelScope.launch {
+            authViewModel.userId?.let { notificationsService.deleteAllNotifications(it) }
+                ?.onFailure { error ->
+                    errorViewModel.showError(
+                        error.message ?: "Error deleting all notifications"
+                    )
+                }
+        }
+    }
 }
