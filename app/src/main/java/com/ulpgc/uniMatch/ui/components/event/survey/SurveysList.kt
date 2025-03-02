fdf448209eda91161e4cecbf733fce110d0351d5
MiguelEscSan <miguelescobedosantana2@gmail.com>
2025-03-02 16:13:23 +0000

    fix: delete surveys

--- app/src/main/java/com/ulpgc/uniMatch/ui/components/event/survey/SurveysList.kt
+++ app/src/main/java/com/ulpgc/uniMatch/ui/components/event/survey/SurveysList.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ulpgc.uniMatch.data.domain.models.Survey
 import com.ulpgc.uniMatch.ui.components.event.EventSurveyCard
+import androidx.compose.runtime.*
 
 @Composable
 fun SurveysList(
@@ -15,18 +16,27 @@ fun SurveysList(
     onDeleteSurveyClick: (Survey) -> Unit,
     onConfirmSurveyClick: (Survey, String, List<String>) -> Unit
 ) {
-    Log.i("SurveysList", "SurveysList $surveys")
-    surveys.forEach { survey ->
-        EventSurveyCard(
-            survey = survey,
-            isEditing = true,
-            onDeleteSurvey = {
-                onDeleteSurveyClick(survey)
-            },
-            onConfirmSurvey = { title, options ->
-                onConfirmSurveyClick(survey, title, options)
-            }
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+    var surveyList by remember { mutableStateOf(surveys) }
+
+    LaunchedEffect(surveys) {
+        surveyList = surveys
+    }
+
+    Log.i("SurveysList", "SurveysList $surveyList")
+
+    surveyList.forEach { survey ->
+        key(survey.title) {
+            EventSurveyCard(
+                survey = survey,
+                isEditing = true,
+                onDeleteSurvey = {
+                    onDeleteSurveyClick(survey)
+                },
+                onConfirmSurvey = { title, options ->
+                    onConfirmSurveyClick(survey, title, options)
+                }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
     }
 }
