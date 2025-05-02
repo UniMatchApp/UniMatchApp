9ae7343dc8e90eed4d4d8281047adfdd46cf39f3
Javier Dávila Rodríguez <60230696+javi9davi@users.noreply.github.com>
2025-05-02 21:29:28 +0100

    EventSection.kt updated

--- app/src/main/java/com/ulpgc/uniMatch/ui/components/event/EventSection.kt
+++ app/src/main/java/com/ulpgc/uniMatch/ui/components/event/EventSection.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -27,7 +28,7 @@ fun EventSection(
 ) {
     val context = LocalContext.current
 
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = Modifier.fillMaxWidth().padding(top = 8.dp)) {
         Text(text = label, style = MaterialTheme.typography.titleMedium)
         Spacer(modifier = Modifier.height(4.dp))
         OutlinedTextField(
